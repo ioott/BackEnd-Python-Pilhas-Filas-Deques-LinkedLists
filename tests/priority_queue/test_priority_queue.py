bf94cc6601c173fe5ce1118258d2c67e5411ecb4
ioott <vioott@gmail.com>
2023-03-08 15:37:38 -0300

    test: Implementa os testes para a classe 'PriorityQueue', capaz de armazenar arquivos pequenos de forma prioritária.

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,31 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    first_mock = {
+        "nome_do_arquivo": "statics/novo_paradigma_globalizado-min.txt",
+        "qtd_linhas": 10,
+        "linhas_do_arquivo": ["linhas"],
+    }
+    second_mock = {
+        "nome_do_arquivo": "statics/arquivo_teste.txt",
+        "qtd_linhas": 3,
+        "linhas_do_arquivo": ["linhas"],
+    }
+
+    priority_queue = PriorityQueue()
+
+    with pytest.raises(IndexError):
+        priority_queue.search(-1)
+
+    priority_queue.enqueue(first_mock)
+    priority_queue.enqueue(second_mock)
+    assert len(priority_queue) == 2
+
+    priority_queue.enqueue(first_mock)
+    assert priority_queue.search(0) == second_mock
+    assert priority_queue.search(1) == first_mock
+
+    priority_queue.dequeue()
+    assert len(priority_queue) == 2
