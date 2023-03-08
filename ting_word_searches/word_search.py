acaf8bd04be50157af8ee55100be811b6e271d74
ioott <vioott@gmail.com>
2023-03-08 12:09:00 -0300

    feat: Implementa a função 'search_by_word' dentro do módulo 'word_search', que busca uma palavra em todos os arquivos processados.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -21,4 +21,23 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = {}
+
+    for item in instance._data:
+        nome_do_arquivo = item['nome_do_arquivo']
+        linhas_do_arquivo = item['linhas_do_arquivo']
+
+        for index, line in enumerate(linhas_do_arquivo):
+            if word.lower() in line.lower():
+                if nome_do_arquivo not in result:
+                    result[nome_do_arquivo] = {
+                        'arquivo': nome_do_arquivo,
+                        'ocorrencias': [],
+                        'palavra': word,
+                    }
+                result[nome_do_arquivo]['ocorrencias'].append({
+                    'conteudo': line,
+                    'linha': index + 1
+                })
+
+    return list(result.values())
